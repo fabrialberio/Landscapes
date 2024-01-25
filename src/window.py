67637b33f6d1b442e7daddf6fe918bba18d63328
Fabri210 <fabrizio.alberio@gmail.com>
2024-01-25 01:05:13 +0100

    Updated interface

--- src/window.py
+++ src/window.py
@@ -17,14 +17,80 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from .parser import WallpaperElement, load_wallpapers
+from .wallpaper_utils import LANDSCAPES_FILE, get_thumbnail_path, wallpaper_files
+
 from gi.repository import Adw, Gtk
+
+
+def update_pref_group(pref_group: Adw.PreferencesGroup, children: list[Gtk.Widget]):
+    listbox = pref_group.get_first_child().get_last_child().get_first_child()
+
+    listbox.remove_all()
+
+    for child in children:
+        pref_group.add(child)
+
+class WallpaperThumbnail(Gtk.Box):
+    '''
+    A thumbnail widget for a wallpaper.
+    
+    Loads the image asynchronously and shows a spinner while loading to avoid
+    blocking the UI.
+    '''
+    __gtype_name__ = 'LandscapeThumbnail'
+
+    def __init__(self, wallpaper: WallpaperElement):
+        super().__init__()
+
+        image = Gtk.Image(
+            file=get_thumbnail_path(wallpaper),
+            pixel_size=96,
+        )
+        image.add_css_class('icon-dropshadow')
+
+        super().append(image)
+
+class WallapaperTile(Adw.ActionRow):
+    __gtype_name__ = 'LandscapeTile'
+
+    def __init__(self, wallpaper: WallpaperElement):
+        super().__init__(
+            title=wallpaper.name,
+        )
+        super().add_prefix(WallpaperThumbnail(wallpaper))
 
 
 @Gtk.Template(resource_path='/io/github/fabrialberio/landscapes/ui/window.ui')
 class Window(Adw.ApplicationWindow):
     __gtype_name__ = 'Window'
 
-    label = Gtk.Template.Child()
+    pref_group_your_backgrounds = Gtk.Template.Child()
+    pref_group_system_backgrounds = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        your_backgrounds = load_wallpapers(LANDSCAPES_FILE)
+        system_backgrounds = []
+
+        for file in wallpaper_files():
+            system_backgrounds.extend(load_wallpapers(file))
+
+        status_page_no_wallpapers = Adw.StatusPage(
+            icon_name='document-open-symbolic',
+            title='No backgrounds',
+            height_request=200,
+        )
+        status_page_no_wallpapers.add_css_class('compact')
+
+        if len(your_backgrounds) > 0:
+            update_pref_group(self.pref_group_your_backgrounds, [
+                WallapaperTile(wallpaper) for wallpaper in your_backgrounds
+            ])
+        else:
+            update_pref_group(self.pref_group_your_backgrounds, [status_page_no_wallpapers])
+
+        update_pref_group(self.pref_group_system_backgrounds, [
+            WallapaperTile(wallpaper) for wallpaper in system_backgrounds
+        ])
